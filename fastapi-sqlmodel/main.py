15c40cfd509e3957bec1dc4087736b9c482dce3e
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-10-24 16:08:50 +0100

    Add: anotated  types

--- fastapi-sqlmodel/main.py
+++ fastapi-sqlmodel/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query
 from schemas import GenreURLChoices, BandBase, BandCreate, BandWithID
+from typing import Annotated 
 
 app = FastAPI()
 
@@ -24,12 +25,17 @@ BANDS = [
             'release_date': '1993-06-01'
         }]
     },
-    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-hop'},
 ]
 
 
 @app.get('/bands')
-async def bands(genre: GenreURLChoices | None = None, has_albums: bool = False) -> list[BandWithID]:
+async def bands(
+        genre: GenreURLChoices | None = None,         
+        has_albums: bool = False,
+        q: Annotated[str | None, Query(max_length =10)] = None,
+) -> list[BandWithID]:
+
     # return BANDS
     band_list = [ BandWithID(**b) for b in BANDS ]
 
@@ -39,11 +45,16 @@ async def bands(genre: GenreURLChoices | None = None, has_albums: bool = False) 
     if has_albums:
         band_list = [ b for b in band_list if len(b.albums) > 0]
 
+    if q: 
+        band_list = [
+            b for b in band_list if q.lower() in b.name.lower()
+        ]
+
     return band_list
 
 
 @app.get('/bands/{band_id}')
-async def band(band_id: int) -> BandWithID:
+async def band(band_id: Annotated[int, Path(description="The band ID")]) -> BandWithID:
     band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         return HTTPException(status_code=404, detail='Band not found')
